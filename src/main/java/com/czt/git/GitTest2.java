519c74a313e919c42efcf08e2b2ff1fc87971a8d
T6001634 <2900861918@qq.com>
2021-05-01 16:22:49 +0800

    提交

---
+++ src/main/java/com/czt/git/GitTest2.java
@@ -0,0 +1,17 @@
+package com.czt.git;
+
+
+/**
+ * @Auther CZT
+ * @Date 2021/5/1
+ */
+public class GitTest2 {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+        System.out.println("hello git2");
+        System.out.println("hello git3");
+        System.out.println("hello git4");
+        System.out.println("hello git hot-fix");
+        System.out.println("hello git master   test");
+    }
+}
